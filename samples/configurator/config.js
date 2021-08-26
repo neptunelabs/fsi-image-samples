854f978d35dbb82c0d7101adba32d993c6a31101
biernat <Manimo12#git>
2021-08-26 16:56:12 +0200

    working on product config

--- samples/configurator/config.js
+++ samples/configurator/config.js
@@ -1,39 +1,48 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-  initClick()
+  new Configurator()
 })
 
+class Configurator {
+  constructor () {
+    this.leathervalue = "4,66,10";
+    this.suedevalue = "0,24,0";
+    this.highlightvalue = "10,50,0";
+    initClick()
+  }
+}
+
 function initClick() {
+  const self = this
   document.querySelectorAll('[data-role]').forEach(function (el) {
     el.addEventListener('click', () => {
-      changeColor(el)
+      self.changeColor(el)
     })
   })
 }
+
 
 
 function changeColor(el) {
   let img;
   let curImage = document.getElementById('image');
   var imgbase = "//fsi-site.neptunelabs.com/fsi/server?type=image&source=images/samples/ssi/configurator/config-shoe-by-artem-bondarchuk.tif&width=940&effects=";
-  let leathervalue = "4,66,10";
-  let suedevalue = "0,24,0";
-  let highlightvalue = "10,50,0";
+
   switch (el.dataset.role) {
     case 'leatherChange':
-      leathervalue = el.dataset.value;
+      this.leathervalue = el.dataset.value;
       break
     case 'suedeChange':
-      suedevalue = el.dataset.value;
+      this.suedevalue = el.dataset.value;
       break
     case 'highlightChange':
-      highlightvalue = el.dataset.value;
+      this.highlightvalue = el.dataset.value;
       break
   }
 
-  console.log('leather selected: ', leathervalue);
-  console.log('suede selected: ', suedevalue);
-  console.log('hightlight selected: ', highlightvalue);
-  img = imgbase + 'select(New,Alpha,1),colorize(' + leathervalue + '),select(New,Alpha,2),colorize(' + suedevalue + '),select(New,Alpha,3),colorize(' + highlightvalue + ')';
+  console.log('leather selected: ', this.leathervalue);
+  console.log('suede selected: ', this.suedevalue);
+  console.log('hightlight selected: ', this.highlightvalue);
+  img = imgbase + 'select(New,Alpha,1),colorize(' + this.leathervalue + '),select(New,Alpha,2),colorize(' + this.suedevalue + '),select(New,Alpha,3),colorize(' + this.highlightvalue + ')';
   console.log('image is:', img)
   curImage.src = img;
 }
