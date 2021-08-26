62641b61bc0773203c8364f66f09f6773d2c6f75
biernat <Manimo12#git>
2021-08-26 17:37:13 +0200

    added two additional alpha channels, switched back to accordion

--- samples/configurator/config.js
+++ samples/configurator/config.js
@@ -7,6 +7,8 @@ class Configurator {
     this.leathervalue = "4,66,10";
     this.suedevalue = "0,24,0";
     this.highlightvalue = "10,50,0";
+    this.highlighttwovalue = "10,24,0";
+    this.leathertwovalue = "11,17,0";
     initClick()
   }
 }
@@ -31,14 +33,20 @@ function changeColor(el) {
     case 'leatherChange':
       this.leathervalue = el.dataset.value;
       break
+    case 'leatherTwoChange':
+      this.leathertwovalue = el.dataset.value;
+      break
     case 'suedeChange':
       this.suedevalue = el.dataset.value;
       break
     case 'highlightChange':
       this.highlightvalue = el.dataset.value;
       break
+    case 'highlightTwoChange':
+      this.highlighttwovalue = el.dataset.value;
+      break
   }
 
-  img = imgbase + 'select(New,Alpha,1),colorize(' + this.leathervalue + '),select(New,Alpha,2),colorize(' + this.suedevalue + '),select(New,Alpha,3),colorize(' + this.highlightvalue + ')';
+  img = imgbase + 'select(New,Alpha,1),colorize(' + this.leathervalue + '),select(New,Alpha,2),colorize(' + this.suedevalue + '),select(New,Alpha,3),colorize(' + this.highlightvalue + '),select(New,Alpha,4),colorize(' + this.leathertwovalue + '),select(New,Alpha,5),colorize(' + this.highlighttwovalue + ')';
   curImage.src = img;
 }
