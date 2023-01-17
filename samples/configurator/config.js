fb217b0a47fa5f51427da84d463733d19d4f86b6
Diana <biernat@neptunelabs.com>
2023-01-17 17:48:26 +0100

    in progress, simplifying configurator (only Leather Upper 1 so far) -> next step: outsourcing array to JSON, changing everything for the other fields (Suede, Highlights, etc)

--- samples/configurator/config.js
+++ samples/configurator/config.js
@@ -4,8 +4,57 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
 class Configurator {
   constructor () {
+    addElements()
     initClick()
   }
+}
+
+function addElements() {
+  const colorSet = [
+    { Name: "Cream", Class: "cream", colorValue: "26,90,55" },
+    { Name: "Coffee", Class: "coffee", colorValue: "11,17,0" },
+    { Name: "Salmon", Class: "salmon", colorValue: "4,66,10" },
+    { Name: "Orange", Class: "orange", colorValue: "10,100,0" },
+    { Name: "Berry", Class: "berry", colorValue: "342,98,17" },
+    { Name: "Mauve", Class: "mauve", colorValue: "227,90,55" },
+    { Name: "Purple", Class: "purple", colorValue: "238,83,00" },
+    { Name: "Blue", Class: "blue", colorValue: "205,100,0" },
+    { Name: "Fresh", Class: "fresh", colorValue: "167,83,0" },
+    { Name: "Lime", Class: "lime", colorValue: "114,83,0" },
+    { Name: "Sun", Class: "sun", colorValue: "55,68,13" }
+  ];
+
+  var content = document.getElementById('leather');
+
+  colorSet.map((colorSet, index) => {
+    var Name = colorSet.Name;
+    var parentDiv = document.createElement('div');
+    var pillDiv = document.createElement('div');
+    let colorCl;
+    colorCl = "icon-" + colorSet.Class + "-round";
+    parentDiv.setAttribute('class', 'position-relative');
+    pillDiv.setAttribute('class', 'position-absolute badge rounded-pill color-badge');
+
+    var Node = document.createTextNode(Name);
+    pillDiv.appendChild(Node);
+    parentDiv.appendChild(pillDiv);
+
+    var Input = document.createElement('input');
+    Input.setAttribute('type', 'radio');
+    Input.setAttribute('name', 'btnradio');
+    Input.setAttribute('id', Name);
+    Input.setAttribute('class', 'btn-check');
+    Input.setAttribute('autocomplete', 'off');
+    parentDiv.appendChild(Input);
+
+    var Label = document.createElement('label');
+    Label.setAttribute('class', colorCl + " btn-circle btn-round me-4");
+    Label.setAttribute('for', Name);
+    Label.setAttribute('data-role', 'leatherChange');
+    Label.setAttribute('data-value', colorSet.colorValue);
+    parentDiv.appendChild(Label);
+    content.appendChild(parentDiv);
+  });
 }
 
 function initClick() {
@@ -43,3 +92,6 @@ function changeColor(el) {
   img = imgbase + 'select(New,Alpha,1),colorize(' + this.leathervalue + '),select(New,Alpha,2),colorize(' + this.suedevalue + '),select(New,Alpha,3),colorize(' + this.highlightvalue + '),select(New,Alpha,4),colorize(' + this.leathertwovalue + '),select(New,Alpha,5),colorize(' + this.highlighttwovalue + ')';
   curImage.src = img;
 }
+
+
+
