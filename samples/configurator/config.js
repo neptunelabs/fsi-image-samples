f2e7d043dbe067439eac0f6e315b30b9ae9adf60
biernat <biernat@neptunelabs.com>
2023-01-19 11:05:05 +0100

    minor changes

--- samples/configurator/config.js
+++ samples/configurator/config.js
@@ -11,48 +11,48 @@ class Configurator {
 
 function addElements() {
   const colorSet = [
-    { Name: "Cream", Class: "cream", colorValue: "26,90,55" },
-    { Name: "Coffee", Class: "coffee", colorValue: "11,17,0" },
-    { Name: "Salmon", Class: "salmon", colorValue: "4,66,10" },
-    { Name: "Orange", Class: "orange", colorValue: "10,100,0" },
-    { Name: "Berry", Class: "berry", colorValue: "342,98,17" },
-    { Name: "Mauve", Class: "mauve", colorValue: "227,90,55" },
-    { Name: "Purple", Class: "purple", colorValue: "238,83,00" },
-    { Name: "Blue", Class: "blue", colorValue: "205,100,0" },
-    { Name: "Fresh", Class: "fresh", colorValue: "167,83,0" },
-    { Name: "Lime", Class: "lime", colorValue: "114,83,0" },
-    { Name: "Sun", Class: "sun", colorValue: "55,68,13" }
+    { name: "Cream", class: "cream", colorValue: "26,90,55" },
+    { name: "Coffee", class: "coffee", colorValue: "11,17,0" },
+    { name: "Salmon", class: "salmon", colorValue: "4,66,10" },
+    { name: "Orange", class: "orange", colorValue: "10,100,0" },
+    { name: "Berry", class: "berry", colorValue: "342,98,17" },
+    { name: "Mauve", class: "mauve", colorValue: "227,90,55" },
+    { name: "Purple", class: "purple", colorValue: "238,83,00" },
+    { name: "Blue", class: "blue", colorValue: "205,100,0" },
+    { name: "Fresh", class: "fresh", colorValue: "167,83,0" },
+    { name: "Lime", class: "lime", colorValue: "114,83,0" },
+    { name: "Sun", class: "sun", colorValue: "55,68,13" }
   ];
 
   let content = document.getElementById('leather');
 
   colorSet.map((colorSet, index) => {
-    let Name = colorSet.Name;
+    let name = colorSet.name;
     let parentDiv = document.createElement('div');
     let pillDiv = document.createElement('div');
     let colorCl;
-    colorCl = "icon-" + colorSet.Class + "-round";
+    colorCl = "icon-" + colorSet.class + "-round";
     parentDiv.setAttribute('class', 'position-relative');
     pillDiv.setAttribute('class', 'position-absolute badge rounded-pill color-badge');
 
-    let Node = document.createTextNode(Name);
-    pillDiv.appendChild(Node);
+    let node = document.createTextNode(name);
+    pillDiv.appendChild(node);
     parentDiv.appendChild(pillDiv);
 
-    let Input = document.createElement('input');
-    Input.setAttribute('type', 'radio');
-    Input.setAttribute('name', 'btnradio');
-    Input.setAttribute('id', Name);
-    Input.setAttribute('class', 'btn-check');
-    Input.setAttribute('autocomplete', 'off');
-    parentDiv.appendChild(Input);
+    let input = document.createElement('input');
+    input.setAttribute('type', 'radio');
+    input.setAttribute('name', 'btnradio');
+    input.setAttribute('id', name);
+    input.setAttribute('class', 'btn-check');
+    input.setAttribute('autocomplete', 'off');
+    parentDiv.appendChild(input);
 
-    let Label = document.createElement('label');
-    Label.setAttribute('class', colorCl + " btn-circle btn-round me-4");
-    Label.setAttribute('for', Name);
-    Label.setAttribute('data-role', 'leatherChange');
-    Label.setAttribute('data-value', colorSet.colorValue);
-    parentDiv.appendChild(Label);
+    let label = document.createElement('label');
+    label.setAttribute('class', colorCl + " btn-circle btn-round me-4");
+    label.setAttribute('for', name);
+    label.setAttribute('data-role', 'leatherChange');
+    label.setAttribute('data-value', colorSet.colorValue);
+    parentDiv.appendChild(label);
     content.appendChild(parentDiv);
   });
 }
