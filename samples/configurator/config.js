c71d57178b037cbb157617783a754f62a4667db7
Steve Alberty <43743955+staybyte@users.noreply.github.com>
2023-01-19 23:58:33 +0100

    [skip ci]

--- samples/configurator/config.js
+++ samples/configurator/config.js
@@ -9,74 +9,70 @@ const imgWidth = 660
 const clipColors = []
 
 const colors = {
-  cream: { name: 'Cream', rgb: '251,217,193' },
-  coffee: { name: 'Coffee', rgb: '150,115,106' },
-  salmon: { name: 'Salmon', rgb: '227,134,126' },
-  orange: { name: 'Orange', rgb: '253,106,75' },
-  berry: { name: 'Berry', rgb: '252,118,159' },
-  mauve: { name: 'Mauve', rgb: '185,202,249' },
-  purple: { name: 'Purple', rgb: '143,146,243' },
-  blue: { name: 'Blue', rgb: '63,173,252' },
-  fresh: { name: 'Fresh', rgb: '96,233,203' },
-  lime: { name: 'Lime', rgb: '110,245,92' },
-  sun: { name: 'Sun', rgb: '248,233,115' },
-  sand: { name: 'Sand', rgb: '183,140,138' },
-  ivory: { name: 'Ivory', rgb: '218,198,194' },
-  rose: { name: 'Rose', rgb: '236,209,197' },
+  cream: {name: 'Cream', rgb: '251,217,193', hsb: '26,90,55'},
+  coffee: {name: 'Coffee', rgb: '150,115,106', hsb: '11,17,0'},
+  salmon: {name: 'Salmon', rgb: '227,134,126', hsb: '4,66,10'},
+  orange: {name: 'Orange', rgb: '253,106,75', hsb: '10,100,0'},
+  berry: {name: 'Berry', rgb: '252,118,159', hsb: '342,98,17'},
+  mauve: {name: 'Mauve', rgb: '185,202,249', hsb: '227,90,55'},
+  purple: {name: 'Purple', rgb: '143,146,243', hsb: '238,83,0'},
+  blue: {name: 'Blue', rgb: '63,173,252', hsb: '205,100,0'},
+  fresh: {name: 'Fresh', rgb: '96,233,203', hsb: '167,83,0'},
+  lime: {name: 'Lime', rgb: '110,245,92', hsb: '114,83,0'},
+  sun: {name: 'Sun', rgb: '248,233,115', hsb: '55,68,13'},
 }
 
 const colorSets = [{
-  desc: 'Leather Upper 1',
+  desc: 'Back',
   img: {
     src: 'leather-upper.png', height: 44,
   },
   clippingPath: 1,
-  selected: colors.cream,
-  colors: [colors.cream, colors.coffee, colors.salmon, colors.orange, colors.berry, colors.mauve, colors.purple, colors.blue, colors.fresh, colors.lime, colors.sun],
+  selected: colors.blue,
+  colors: [colors.cream, colors.coffee, colors.salmon, colors.orange, colors.berry, colors.mauve, colors.purple, colors.blue, colors.fresh, colors.lime, colors.sun,],
 }, {
-  desc: 'Leather Upper 2',
+  desc: 'Suede',
   img: {
     src: 'leather-upper1.png', height: 44,
   },
   clippingPath: 2,
-  selected: colors.coffee,
-  colors: [colors.coffee, colors.salmon, colors.orange, colors.berry, colors.mauve, colors.purple, colors.blue, colors.fresh, colors.lime, colors.sun],
+  selected: colors.blue,
+  colors: [colors.coffee, colors.salmon, colors.berry, colors.mauve, colors.blue, colors.sun,],
 }, {
-  desc: 'Suede',
+  desc: 'Finish',
   img: {
     src: 'suede-upper.png', height: 44,
   },
   clippingPath: 3,
-  selected: colors.salmon,
-  colors: [colors.cream, colors.coffee, colors.salmon, colors.orange, colors.berry, colors.mauve, colors.purple, colors.blue, colors.fresh, colors.lime, colors.sun],
+  selected: colors.orange,
+  colors: [colors.coffee, colors.salmon, colors.orange, colors.berry, colors.mauve, colors.blue, colors.fresh, colors.lime],
 }, {
-  desc: ' Highlight 1',
+  desc: ' Velvet',
   img: {
     src: 'highlight.png', height: 44,
   },
   clippingPath: 4,
-  selected: colors.berry,
-  colors: [colors.cream, colors.coffee, colors.salmon, colors.orange, colors.berry, colors.mauve, colors.purple, colors.blue, colors.fresh, colors.lime, colors.sun],
+  selected: colors.blue,
+  colors: [colors.cream, colors.coffee, colors.salmon, colors.berry, colors.mauve, colors.blue, colors.fresh, colors.sun,],
 }, {
-  desc: ' Highlight 2',
+  desc: ' Side',
   img: {
     src: 'highlight1.png', height: 44,
   },
   clippingPath: 5,
-  selected: colors.mauve,
+  selected: colors.orange,
   colors: [colors.cream, colors.coffee, colors.salmon, colors.orange, colors.berry, colors.mauve, colors.purple, colors.blue, colors.fresh, colors.lime, colors.sun],
-}]
+},]
 
 const addElements = () => {
-
   const colorSelector = document.getElementById('colorSelector')
 
   for (const area of colorSets) {
     const selectorEl = document.createElement('div')
-    selectorEl.className = 'pb-1 border-custom'
+    selectorEl.className = 'row'
 
     const headlineCntEl = document.createElement('div')
-    headlineCntEl.className = 'pb-3'
+    headlineCntEl.className = 'col-3 pb-3'
     selectorEl.appendChild(headlineCntEl)
 
     const headlineImgEl = document.createElement('img')
@@ -85,24 +81,27 @@ const addElements = () => {
     headlineImgEl.setAttribute('src', baseImgPath + area.img.src + '&height=' + area.img.height)
     headlineCntEl.appendChild(headlineImgEl)
 
-    const headlineTxtEl = document.createElement('span')
-    headlineTxtEl.className = 'm-4'
+    const headlineTxtEl = document.createElement('h1')
+    headlineTxtEl.className = 'display-6'
     headlineTxtEl.innerText = area.desc
     headlineCntEl.appendChild(headlineTxtEl)
 
     // <div aria-label='Leather 2 Upper Buttons' class='btn-group pb-3' id='leather2' role='group'>
     const colorsEl = document.createElement('div')
-    colorsEl.className = 'btn-group pb-3'
+    colorsEl.className = 'col-9 btn-group pb-3'
     colorsEl.setAttribute('role', 'group')
     selectorEl.appendChild(colorsEl)
 
-    for (const color of area.colors) {
+    const colorsElSub = document.createElement('div')
+    colorsElSub.className = 'container-fluid'
+    colorsEl.appendChild(colorsElSub)
 
+    for (const color of area.colors) {
       const colorWrapEl = document.createElement('div')
-      colorWrapEl.className = 'position-relative'
+      colorWrapEl.className = 'd-inline-flex position-relative'
 
       const labelNameEl = document.createElement('div')
-      labelNameEl.className = 'position-absolute badge rounded-pill color-badge'
+      labelNameEl.className = 'opacity-75 badge text-bg-secondary position-absolute top-0 center translate-middle rounded-pill'
       labelNameEl.innerText = color.name
       colorWrapEl.appendChild(labelNameEl)
 
@@ -111,35 +110,37 @@ const addElements = () => {
       radioEl.setAttribute('type', 'radio')
       radioEl.name = 'radio-grp-' + area.clippingPath
       radioEl.className = 'btn-check'
-      if (area.selected == color) {
+      if (area.selected === color) {
         radioEl.checked = true
-        clipColors[area.clippingPath] = rgbToColorize(color.rgb)
+        clipColors[area.clippingPath] = color.hsb
       }
       radioEl.addEventListener('click', () => {
-        colorizeImage(area.clippingPath, color.rgb)
+        colorizeImage(area.clippingPath, color.hsb)
       })
       colorWrapEl.appendChild(radioEl)
 
       const labelEl = document.createElement('label')
-      labelEl.className = 'btn-circle btn-round me-4'
+      labelEl.className = 'demo-btn-circle me-4'
       labelEl.style.backgroundColor = 'rgb(' + color.rgb + ')'
       labelEl.setAttribute('for', radioEl.id)
       colorWrapEl.appendChild(labelEl)
 
-      colorsEl.appendChild(colorWrapEl)
-
+      colorsElSub.appendChild(colorWrapEl)
     }
 
     colorSelector.appendChild(selectorEl)
+
+    const dividerEl = document.createElement('hr')
+    dividerEl.className = 'mt-1 mb-5'
+    colorSelector.appendChild(dividerEl)
   }
+
 }
 
-const colorizeImage = (clippingPath, rgbStr) => {
-
+const colorizeImage = (clippingPath, hsbStr) => {
   let imgEl = document.getElementById('image')
 
-  if (clippingPath && rgbStr) {
-    const hsbStr = rgbToColorize(rgbStr)
+  if (clippingPath && hsbStr) {
     clipColors[clippingPath] = hsbStr
   }
 
@@ -152,28 +153,4 @@ const colorizeImage = (clippingPath, rgbStr) => {
   imgSrc += '&effects=' + clipEffects.join(',')
 
   imgEl.src = imgSrc
-
-}
-
-const rgbToColorize = (rgbStr) => {
-
-  const rgbSplit = rgbStr.split(',')
-
-  const r = rgbSplit[0] / 255
-  const g = rgbSplit[1] / 255
-  const b = rgbSplit[2] / 255
-
-  let l = Math.max(r, g, b)
-  let s = l - Math.min(r, g, b)
-  let h = s ? l === r ? (g - b) / s : l === g ? 2 + (b - r) / s : 4 + (r - g) / s : 0
-
-  const res = [
-    Math.round(60 * h < 0 ? 60 * h + 360 : 60 * h),
-    Math.round(100 * (s ? (l <= 0.5 ? s / (2 * l - s) : s / (2 - (2 * l - s))) : 0)),
-    Math.round((100 * (2 * l - s)) / 2)
-  ]
-
-  res[2] = 0
-
-  return res.join(',')
 }
